d9fda6622a6927716470bb320128ce8197e048ed
shriram tiwari <shriramt.124@gmail.com>
2024-03-02 07:05:26 +0000

    error fixed of custom routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import CustomRoutes from './routes/customRoutes.jsx'
+ 
+import CustomRoutes from "./routes/CustomRoutes"
 import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
